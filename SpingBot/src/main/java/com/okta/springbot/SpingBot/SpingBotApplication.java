d0a6f908c02559e54fea06b1e420757d300e16cc
Daniil Lifanov <lifanovdaniil@gmail.com>
2024-02-25 00:24:00 +0400

    Initial commit

---
+++ SpingBot/src/main/java/com/okta/springbot/SpingBot/SpingBotApplication.java
@@ -0,0 +1,13 @@
+package com.okta.springbot.SpingBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBotApplication.class, args);
+	}
+
+}
